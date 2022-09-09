46baed28aaee9e85206c95c0bdf59194e6e5e3a3
Snake Nguyen <huho@deakin.edu.au>
2022-09-09 23:16:26 +1000

    newUp

--- prac7/main.js
+++ prac7/main.js
@@ -1,1 +1,33 @@
-new Vue({ el: '#components-demo' })
+Vue.component('component1', {
+    template: '<div>{{message}}</div>',
+    data: function () {
+        return {
+            message: 'This is our global extermination.',
+        };
+    },
+});
+
+var component2 = {
+    template: '<div>This is our local extermination</div>',
+};
+
+Vue.component('kimChiNam', {
+    props: ['line'],
+    template: '<div>{{line}}</div>',
+});
+
+var app = new Vue({
+    el: '#app',
+    components: {
+        'component2': component2,
+    },
+    data: {
+        lyrics: [
+            {id:1, line:'In-rhymes is not a skill?'},
+            {id:2, line:'Meta is not a skill?'},
+            {id:3, line:'In-rhymes is not a skill?'},
+            {id:4, line:'So I wonder whether your grip is not as real.'},
+        ],
+    },
+});
+
